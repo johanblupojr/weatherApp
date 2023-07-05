8c740dfb87a41977d35b5c2669232f20402840cf
johanblupojr <johanblupojr@gmail.com>
2023-07-06 00:33:36 +0800

    Delete messenger

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Weather from './Components/weather'
 import './Components/CSS/weather.css'
-import Messenger from './Components/messenger'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Messenger />
+
     <Weather />
     </>
   )
