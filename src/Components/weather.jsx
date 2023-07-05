0c2b03a1cfa241990b18a264ff4d31ef797f04e9
johanblupojr <johanblupojr@gmail.com>
2023-07-06 00:05:25 +0800

    Add: Messenger plugin

--- src/Components/weather.jsx
+++ src/Components/weather.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function Weather() {
   const [data, setData] = useState({
@@ -71,6 +72,10 @@ function Weather() {
           </div>
         </div>
       </div>
+      <MessengerCustomerChat
+    pageId="108965818922829"
+    appId="277041771537219"
+  />,
     </div>
   );
 }
